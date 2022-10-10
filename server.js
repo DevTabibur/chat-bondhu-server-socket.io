75de7f33312a4c9047b2989418ded7840ebfdc26
unknown <tobiburrohman2@gmail.com>
2022-10-10 08:40:39 +0600

    last commit

--- server.js
+++ server.js
@@ -1,43 +1,11 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const expressServer = http.createServer(app);
-
-// const cors = require("cors");
-// app.use(cors());
-// const { Server } = require("socket.io");
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// app.get("/", async function (req, res) {
-//   res.send("This is backend");
-// });
-
-// io.on("connection", function (socket) {
-//   console.log("New user is connected");
-
-//   // user disconnected
-//   socket.on("disconnect", function () {
-//     console.log("User is disconnected");
-//   });
-// });
-
-// expressServer.listen(port, function (req, res) {
-//   console.log("server is running on port 5000");
-// });
-
-
 const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 
@@ -51,24 +19,46 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+  const id = socket.id;
+
   console.log(`User Connected: ${socket.id}`);
 
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//   });
 
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   });
-
-  socket.on("disconnect", function(){
-    console.log('user is disconnected')
-  })
+  socket.on("disconnect", function () {
+    console.log(`User disconnected: ${socket.id}`);
+  });
 });
 
-app.get("/", async (req, res)=>{
-    res.send("Hello world")
-})
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
+
+// user: chat-bondhu
+// pswd: M3y5wIQPDoaCcIet
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("chat-bondhu").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find({}).toArray();
+      res.send(result);
+    });
+
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
 server.listen(port, () => {
   console.log(`SERVER IS RUNNING AT ${port}`);
 });
